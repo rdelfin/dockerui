ce3007783b8992663739ed53e57e22b61ca1a72a
Ricardo Delfin <me@rdelfin.com>
2020-11-22 22:00:27 +0000

    Fixed cards to have a fixed width

--- src/ContainerCard.tsx
+++ src/ContainerCard.tsx
@@ -5,6 +5,7 @@
 import React from "react";
 import Button from "@material-ui/core/Button";
 import Card from "@material-ui/core/Card";
+import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 import { withStyles, WithStyles, createStyles } from "@material-ui/core/styles";
 import CardActions from "@material-ui/core/CardActions";
@@ -35,20 +36,22 @@ function ContainerCard(props: Props) {
     const { classes, name, host } = props;
 
     return (
-        <Card className={classes.root}>
-            <CardContent>
-                <Typography variant="body2" component="p">
-                    {host}
-                </Typography>
-                <Typography variant="h5" component="h2">
-                    {name}
-                </Typography>
-            </CardContent>
+        <Grid item xs={3}>
+            <Card className={classes.root}>
+                <CardContent>
+                    <Typography variant="body2" component="p">
+                        {host}
+                    </Typography>
+                    <Typography variant="h5" component="h2">
+                        {name}
+                    </Typography>
+                </CardContent>
 
-            <CardActions className={classes.bottom_action}>
-                <Button size="small">More</Button>
-            </CardActions>
-        </Card>
+                <CardActions className={classes.bottom_action}>
+                    <Button size="small">More</Button>
+                </CardActions>
+            </Card>
+        </Grid>
     );
 }
 
